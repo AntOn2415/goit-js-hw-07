d94252069fe45d36f712f644f2546b8371686959
AntOnAntOff <povanton@gmail.com>
2023-04-05 21:24:15 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,21 +25,29 @@ function createGalleryMarkup(galleryItems) {
     .join("");
 }
 
+let instance = null;
+
 function onGalleryElClick(e) {
   e.preventDefault();
   if (e.target.nodeName !== "IMG") {
     return;
   }
-  const instance = basicLightbox.create(`
-    <img src="${e.target.dataset.source}" width="800" height="600">
-  `);
-  instance.show();
-
-  galleryEl.addEventListener("keydown", onEscKeyPress);
-
-  function onEscKeyPress(e) {
-    if (e.code === "Escape") {
-      instance.close(galleryEl.removeEventListener("keydown", onEscKeyPress));
+  instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: instance => {
+        document.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: instance => {
+        document.removeEventListener("keydown", onEscKeyPress);
+      },
     }
+  );
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code === "Escape") {
+    instance.close();
   }
 }
